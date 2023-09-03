5fcc834c9bb1c6f6a73dbbf24902629916c3c726
Фёдоров Ренат <homerw223v@mail.ru>
2023-09-03 16:14:52 +0700

    Add new city

--- city/city.py
+++ city/city.py
@@ -5,3 +5,4 @@ async def create_cities():
     with open('city/City.txt', 'r', encoding='utf-8') as file:
         for city in file:
             cities.append(city.strip().lower())
+    print('-----List of city created')
