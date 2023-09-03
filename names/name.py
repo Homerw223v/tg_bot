0c27351aaca43cad087ee97618573ee925032160
Фёдоров Ренат <homerw223v@mail.ru>
2023-09-03 16:12:24 +0700

    Add format_time variable

--- names/name.py
+++ names/name.py
@@ -3,10 +3,11 @@ five_days = None
 commercial_keys = ['commercial', 'times', 'content_type', 'file_id', 'file_unique_id', 'link', 'button_text']
 publishing: list = ['not_published', 'will_be_published', 'published']
 stories_id = None
+time_format = "%Y-%m-%d %H:%M:%S.%f"
 banned_users = list()
 hiragana = {'あ': 'а', 'い': 'и', 'う': 'у', 'え': 'э', 'お': 'о',
             'か': 'ка', 'き': 'ки', 'く': 'ку', 'け': 'кэ', 'こ': 'ко',
-            'さ': 'сa', 'し': 'си', 'す': 'су', 'せ': 'сэ', 'そ': 'сэ'}
+            'さ': 'са', 'し': 'си', 'す': 'су', 'せ': 'сэ', 'そ': 'со'}
 # 'た': 'тa', 'ち': 'ти', 'つ': 'цу', 'て': 'тэ', 'と': 'то',
 # 'な': 'на', 'に': 'ни', 'ぬ': 'ну', 'ね': 'нэ', 'の': 'но',
 # 'は': 'хa', 'ひ': 'хи', 'ふ': 'фу', 'へ': 'хэ', 'эほ': 'хо',
